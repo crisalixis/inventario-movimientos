2e7c6545114b0dbb6f2ee89ad48c4b3f0c0bb6e2
kande <163929568+kandelita@users.noreply.github.com>
2024-10-17 14:23:55 -0300

    Update conexion.php

--- ingreso_materiales/conexion.php
+++ ingreso_materiales/conexion.php
@@ -1,13 +1,12 @@
 <?php
-$servername = "localhost"; // Cambia según sea necesario
-$username = "root"; // Cambia según sea necesario
-$password = ""; // Cambia según sea necesario
-$dbname = "control_inventario"; // Cambia según sea necesario
+$servername = "localhost"; 
+$username = "root"; 
+$password = "";
+$dbname = "control_inventario"; 
 
 try {
-    // Crear conexión
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // Configurar el modo de error de PDO para que lance excepciones
+
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Conexión fallida: " . $e->getMessage());
