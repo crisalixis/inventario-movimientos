72e9947d4bc35d3bfef437e5d57113538f291307
crisalixis <164426470+crisalixis@users.noreply.github.com>
2024-11-06 20:17:35 -0300

    Update lista-materiales.php

--- material/lista-materiales.php
+++ material/lista-materiales.php
@@ -6,7 +6,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-    <title>Sistema de Inventario</title>
+    <title>Lista de materiales</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
